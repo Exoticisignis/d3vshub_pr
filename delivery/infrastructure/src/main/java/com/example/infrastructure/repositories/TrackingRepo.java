276daf79b13d34143d3eaae771a1c06f38b842c5
Matsvei Dzebysh <debysh99@gmail.com>
2023-05-31 16:52:36 +0200

    Small refactoring

--- delivery/infrastructure/src/main/java/com/example/infrastructure/repositories/TrackingRepo.java
+++ delivery/infrastructure/src/main/java/com/example/infrastructure/repositories/TrackingRepo.java
@@ -1,6 +1,5 @@
 package com.example.infrastructure.repositories;
 
-import com.example.infrastructure.entities.Courier;
 import com.example.infrastructure.entities.Tracking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
