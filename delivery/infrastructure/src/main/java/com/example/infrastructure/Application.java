1da87ee66ec61358e780a31578ac3cf4b5065574
Matsvei Dzebysh <debysh99@gmail.com>
2023-06-20 16:27:20 +0200

    KeycloakLogoutHandler added, RestTemplate added in Application class for KeycloakLogoutHandler

--- delivery/infrastructure/src/main/java/com/example/infrastructure/Application.java
+++ delivery/infrastructure/src/main/java/com/example/infrastructure/Application.java
@@ -3,7 +3,9 @@ package com.example.infrastructure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @EnableScheduling
@@ -11,5 +13,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
